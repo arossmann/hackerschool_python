985f377b70310422930eb379207d64103b680e2a
Arne Rossmann (Work) <arossman@Sleipnir.fritz.box>
2023-04-19 12:13:37 +0200

    small update

--- 04.py
+++ 04.py
@@ -1,3 +1,27 @@
+# string
+name = "Arne"
+
+#integer
+zahl = 42
+
+# float
+pi = 3.14
+
+# bool
+wahr = True
+falsch = False
+
+eingabe = input("nenne mir bitte eine Zahl: ")
+
+if int(eingabe) == zahl:
+  print("Deine Zahl ist 42.")
+elif int(eingabe) == 23:
+  print("23 ist auch eine schöne Zahl.")
+elif int(eingabe) == 9:
+  print("9 ist auch eine nette Zahl.")
+else:
+  print("du hast eine andere Zahl gewählt.")
+
 def malZwei(x):
   return 2*x
 
